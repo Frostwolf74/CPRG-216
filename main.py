b9b693b388000d6e96fc439079812f4fe127ff68
Frost <152447084+Frostwolf74@users.noreply.github.com>
2024-09-17 16:00:54 -0600

    Very basic python

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# test commit
+import math
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello " + name + ", " + age)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter the next number: ")
+
+ans = int(num1) + int(num2)
+print(ans)
